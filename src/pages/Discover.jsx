cc1adf959d64f37139d900857c09a6bc43b73b8c
Aung Myo Oo <dev.aungmyooo2015@gmail.com>
2024-01-10 01:37:18 +0800

    home page almost done.

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,11 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const genreTitle = 'Pop';
   const { data, isFetching, error } = useGetTopChartsQuery();
   if (isFetching) return <Loader />;
@@ -10,9 +13,7 @@ const Discover = () => {
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-3xl text-white text-left">
-          Discover {genreTitle}
-        </h2>
+        <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
         <select className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
           {genres.map((genre) => (
             <option key={genre.value} value={genre.value}>
@@ -24,7 +25,7 @@ const Discover = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.result.tracks.map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+          <SongCard key={song.key} song={song} isPlaying={isPlaying} activeSong={activeSong} i={i} />
         ))}
       </div>
     </div>
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,34 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '127263eadbmsh730ef14e7b9122ap1d606fjsn41a9ea47a1ef',
-    'X-RapidAPI-Host': 'shazam-api6.p.rapidapi.com',
-  },
-};
-
-fetch(
-  'https://shazam-api6.p.rapidapi.com/shazam/top_tracks_country?country_code=UZ&limit=10',
-  options,
-)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
-
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000/shazam',
-    // prepareHeaders: (headers) => {
-    //   headers.set(
-    //     'X-RapidAPI-Key',
-    //     '127263eadbmsh730ef14e7b9122ap1d606fjsn41a9ea47a1ef',
-    //   );
-    //   headers.set('X-RapidAPI-Host', 'localhost');
-
-    //   return headers;
-    // },
+    baseUrl: 'http://localhost:3001/shazam',
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({
